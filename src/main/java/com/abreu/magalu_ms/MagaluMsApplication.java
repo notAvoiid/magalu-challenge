57d05cde765ebc8d54921c45a63ebe724f7a1b6a
Igor Nascimento <igor.nacimento51@gmail.com>
2024-07-05 13:52:20 -0300

    feat: add initial structure

---
+++ src/main/java/com/abreu/magalu_ms/MagaluMsApplication.java
@@ -0,0 +1,13 @@
+package com.abreu.magalu_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abreu/magalu_ms/MagaluMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abreu.magalu_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
